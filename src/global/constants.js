5e1479b70a0dd3ef656c99151bb9b39ae91b1c90
Algoor Turling <1561601523@qq.com>
2023-09-02 18:24:10 +0800

    update

---
+++ src/global/constants.js
@@ -0,0 +1,2 @@
+export const API_HOST = "http://95.169.25.59:3001";
+export const API_GET_DATA = `${API_HOST}/posts/1`;
---
+++ src/pages/Home/components/Edit.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { v4 } from "uuid";
+
+const Edit = ({ add, submittingStatus }) => {
+  const [note, setNote] = useState("");
+  function noteChange(e) {
+    setNote(e.target.value);
+  }
+
+  const [date, setDate] = useState("");
+  function dateChange(e) {
+    setDate(e.target.value);
+  }
+
+  const [time, setTime] = useState("");
+  function timeChange(e) {
+    setTime(e.target.value);
+  }
+
+  function addItem() {
+    submittingStatus.current = true;
+    add(function (prevData) {
+      return [
+        {
+          id: v4(),
+          note,
+          date,
+          time,
+        },
+        ...prevData,
+      ];
+    });
+  }
+
+  return (
+    <div>
+      <h1>备忘录</h1>
+      <p>记事：</p>
+      <input type="text" value={note} onChange={noteChange} />
+      <p>日期：</p>
+      <input type="date" value={date} onChange={dateChange} />
+      <p>时间：</p>
+      <input type="time" value={time} onChange={timeChange} />
+      <button onClick={addItem} className="add">
+        新增
+      </button>
+    </div>
+  );
+};
+
+export default Edit;
